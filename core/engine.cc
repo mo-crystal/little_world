5bc2f95807ad2fdf1b926467cb8a0a1ad2b2528a
Edmond-J-A <edomondja@gmail.com>
2024-05-27 23:35:59 +0800

    engine adjust

--- core/engine.cc
+++ core/engine.cc
@@ -1,12 +1,31 @@
-#include<engine.h>
+#include "engine.h"
 
-Engine::Engine()
+Engine::Engine(int side, std::string _mode)
 {
-  mapcontrollor=new MapControllor;
-  resourcecontrollor=new ResourceControllor("");
+  mode = _mode;
 }
 
 Engine::~Engine()
 {
-  delete mapcontrollor;
+}
+
+void Engine::Init()
+{
+  Point a(480, 320);
+  Player *p = new Player(a);
+  p->InitStates("D:\\CODE\\C++\\civilization\\src\\client\\res\\game\\c1");
+  tmpPlayers.push_back(p);
+}
+
+void Engine::Start()
+{
+  while (true)
+  {
+    std::vector<render_data> datas;
+    for (int i = 0; i < tmpPlayers.size(); i++)
+    {
+      datas.push_back(tmpPlayers[i]->GetRenderData());
+      tmpPlayers[i]->TickAction();
+    }
+  }
 }
--- core/engine.h
+++ core/engine.h
@@ -1,19 +1,24 @@
 #ifndef ENGINE_H
 #define ENGINE_H
 
-#include<./game_map/map_controllor.h>
-#include<./resource_controllor/resource_controllor.h>
+#include "./impl_actors/player.h"
+
+
+#define SERVER 1
+#define CLIENT 2
 
 class Engine
 {
 private:
-  MapControllor *mapcontrollor;
-  ResourceControllor *resourcecontrollor;
+  std::string mode = "release";
+  double TickTime = 0.0167;
+  std::vector<Player*> tmpPlayers;
 public:
-  Engine();
+  Engine(int side = CLIENT, std::string _mode = "release");
   ~Engine();
+  void Init();
+  void Start();
+  void SetTickTime(double time) { TickTime = time; }
 };
-
-
 
 #endif
