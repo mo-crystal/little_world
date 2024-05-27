ca5e569cedb0a2259a1ff60d8d6a1efc0d70119b
Edmond-J-A <edomondja@gmail.com>
2024-05-27 23:34:07 +0800

    add point

--- core/basic/point.cc
+++ core/basic/point.cc
@@ -1,9 +1,28 @@
-#include<point.h>
+#include "point.h"
 
 Point::~Point()
 {
 }
 
-int Point::get_distance(Point &p){
-  return sqrt((this->x-p.get_x())*(this->x-p.get_x())+(this->y-p.get_y())*(this->y-p.get_y()));
+int Point::get_distance(const Point &p) const
+{
+  return sqrt((this->x - p.get_x()) * (this->x - p.get_x()) + (this->y - p.get_y()) * (this->y - p.get_y()));
+}
+
+Point Point::operator+(const Point &p) const
+{
+  return Point(x + p.x, y + p.y);
+}
+Point Point::operator-(const Point &p) const
+{
+  return Point(x - p.x, y - p.y);
+}
+Point &Point::operator=(const Point &other)
+{
+  if (this != &other)
+  {
+    x = other.x;
+    y = other.y;
+  }
+  return *this;
 }
