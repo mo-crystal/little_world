1621c64afb48238fe3466b0c16892cf0e1cd38f2
Edmond-J-A <edomondja@gmail.com>
2024-06-04 16:22:20 +0800

    finish render controller part

--- core/controllers/render_controller.cc
+++ core/controllers/render_controller.cc
@@ -1,21 +1,28 @@
 #include "render_controller.h"
 
-RenderController::RenderController(/* args */)
-{
-}
-
 RenderController::~RenderController()
 {
 }
 
 void RenderController::SetFrameData(const std::vector<render_data> &a)
 {
-  std::lock_guard<std::mutex> lock(mtx); 
+  std::lock_guard<std::mutex> lock(mtx);
   frame_data = a;
 }
 
 void RenderController::Draw()
 {
   std::lock_guard<std::mutex> lock(mtx);
-  
+}
+
+void RenderController::Start(double interval)
+{
+  std::thread render_thread([&]()
+                            {
+            while (true)
+            {
+                std::this_thread::sleep_for(std::chrono::duration<double>(interval));
+                this->Draw();
+            } });
+  render_thread.detach();
 }
--- core/controllers/render_controller.h
+++ core/controllers/render_controller.h
@@ -5,20 +5,22 @@
 #include <mutex>
 #include <chrono>
 
+#include "controller.h"
 #include "../basic/render_data.h"
 
-class RenderController
+class RenderController:public Controller
 {
 private:
   std::vector<render_data> frame_data;
   std::mutex mtx;
 
 public:
-  RenderController(/* args */);
+  RenderController(Engine * e):Controller(e){}
   ~RenderController();
 
   void SetFrameData(const std::vector<render_data> &a);
   void Draw();
+  void Start(double interval);
 };
 
 #endif
